bebd57606d7b7a12abf8c0088c96afe6b5c1e6a1
manhattan <andrey2004uor@gmail.com>
2023-04-02 15:18:03 +0300

    firstCommit

---
+++ DatingApp/Program.cs
@@ -0,0 +1,16 @@
+using DatingApp.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+app.MapControllers();
+
+app.Run();
